758a35b54ad2cbad869f11ae2ef042e5c39658c1
sarmadi <mr.snaros@gmail.com>
2021-02-26 16:16:41 +0330

    add icome and beatiffulation models and gitignore

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Expanse,Income
 # Register your models here.
+
+admin.site.register(Expanse)
+admin.site.register(Income)
+
--- web/models.py
+++ web/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+class Expanse(models.Model):
+        text = models.CharField(max_length=250)
+        date=models.DateTimeField()
+        amount = models.BigIntegerField()
+        users = models.ForeignKey(User , on_delete=models.CASCADE )
+        def __str__(self):
+            return '{}-{}'.format(self.date , self.amount)
+
+
+class Income(models.Model):
+        text = models.CharField(max_length=250)
+        date=models.DateTimeField()
+        amount = models.BigIntegerField()
+        users = models.ForeignKey(User , on_delete=models.CASCADE )
+        def __str__(self):
+            return '{}-{}'.format(self.date , self.amount)
+
